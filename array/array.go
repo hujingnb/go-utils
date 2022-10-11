80929dcd95b8ef8544fb88790f61d7129fcea108
hujingnb <hujingnb@qq.com>
2022-10-11 12:45:05 +0800

    error commit

--- array/array.go
+++ array/array.go
@@ -21,21 +21,3 @@ func Chunk[T any](array []T, size int) ([][]T, error) {
 	}
 	return result, nil
 }
-
-func ArrayChunk(s []interface{}, size int) [][]interface{} {
-	if size < 1 {
-		panic("size: cannot be less than 1")
-	}
-	length := len(s)
-	chunks := int(math.Ceil(float64(length) / float64(size)))
-	var n [][]interface{}
-	for i, end := 0, 0; chunks > 0; chunks-- {
-		end = (i + 1) * size
-		if end > length {
-			end = length
-		}
-		n = append(n, s[i*size:end])
-		i++
-	}
-	return n
-}
